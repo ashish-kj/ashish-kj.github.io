72520dd6768bc66690c8c17067538ddd18cefde7
ashish-kj <jha.ashish.kj@gmail.com>
2025-02-13 00:29:15 -0800

    Initial commit: Template files

---
+++ roleScript.js
@@ -0,0 +1,14 @@
+var typeData = new Typed(".role", {
+    strings: [
+      "a Full Stack Developer",
+      "a Web Developer",
+      "a UI-UX Designer",
+      "a Backend Developer",
+      "a Coder",
+      "Aayush Bharti",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1000,
+  });
